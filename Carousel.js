316220b9a2d12af53cbb521b848a976c166911c8
Rein Van Oyen <rein@rein.tnt.lan>
2015-10-05 10:50:34 +0200

    initial

---
+++ Carousel.js
@@ -0,0 +1,213 @@
+var $ = require( 'jquery' );
+
+var Carousel = function( $element ) {
+	this.$element = $element;
+	this.activeIndex = 0;
+	this.build();
+};
+
+Carousel.prototype = {
+	build: function() {
+
+		var that = this;
+
+		this.loadImages( function()
+		{
+			that.$wrap =  that.$element.wrap( '<div>' ).parent();
+
+			that.$prevButton = $( '<button>' )
+				.addClass( 'carousel-prev-button' )
+				.appendTo( that.$wrap )
+			;
+
+			that.$nextButton = $( '<button>' )
+				.addClass( 'carousel-next-button' )
+				.appendTo( that.$wrap )
+			;
+
+			that.$element.addClass( 'loaded' );
+
+			that.$firstImage = that.$images.eq( 0 );
+			that.refresh();
+			that.bindEvents();
+		} );
+	},
+	loadImages: function( callback ) {
+
+		this.$images = this.$element.find( 'img' );
+
+		this.amountOfSlides = this.$images.size();
+
+		var amountLoaded = 0,
+			amountToLoad = this.amountOfSlides;
+
+		this.$images.each( function()
+		{
+			var $currentImage = $( this );
+			var image = new Image();
+
+			image.onload = function()
+			{
+				amountLoaded++;
+
+				if( amountLoaded === amountToLoad )
+				{
+					callback();
+				}
+			}
+
+			image.src = $currentImage.attr( 'src' );
+		} );
+	},
+	bindEvents: function() {
+
+		var that = this;
+
+		$( window ).resize( function()
+		{
+			that.refresh();
+		} );
+
+		this.$nextButton.click( function()
+		{
+			that.next();
+		} );
+
+		this.$prevButton.click( function()
+		{
+			that.prev();
+		} );
+
+		$( document ).keydown( function( e )
+		{
+			if( e.keyCode == 37 )
+			{
+				that.prev();
+			}
+			else if( e.keyCode == 39 )
+			{
+				that.next();
+			}
+		} );
+
+		this.bindTouchEvents();
+	},
+	bindTouchEvents: function() {
+
+		var that = this;
+
+		var swipeDistance,
+			posX,
+			posY
+		;
+
+		this.$wrap.bind( 'touchstart', function( e ) {
+			var e = e.originalEvent.changedTouches[ 0 ];
+			posX = e.clientX;
+			posY = e.clientY;
+			swipeDistance = 0;
+		} );
+
+		this.$wrap.bind( 'touchmove', function( e ) {
+			e.preventDefault();
+		} );
+
+		this.$wrap.bind( 'touchend', function( e ) {
+			var e = e.originalEvent.changedTouches[ 0 ];
+			swipeDistance = e.clientX - posX;
+			that.processSwipeDistance( swipeDistance );
+		} );
+	},
+	processSwipeDistance: function( dist ) {
+		if( dist < 0 )
+		{
+			this.next();
+		}
+
+		if( dist > 0 )
+		{
+			this.prev();
+		}
+	},
+	refresh: function() {
+		var that = this;
+
+		this.activeIndex = 0;
+
+		clearTimeout( this.timeout );
+
+		this.$element.children( 'li' ).attr( 'style', '' );
+		this.$element.attr( 'style', '' );
+
+		this.elementWidth = this.$wrap.width();
+
+		this.$element.css( {
+			width: this.elementWidth
+		} );
+
+		this.slideWidth = this.$firstImage.width();
+		this.slideHeight = this.$firstImage.height();
+
+		this.$wrap.css( {
+			position: 'relative',
+			overflow: 'hidden',
+			height: this.slideHeight
+		} );
+
+		this.amountVisible = Math.ceil( this.elementWidth / this.slideWidth );
+
+		this.totalWidth = ( this.amountOfSlides * this.slideWidth );
+
+		this.timeout = setTimeout( function()
+		{
+			that.$element.css( {
+				width: that.totalWidth
+			} );
+
+			that.$element.children( 'li' ).css( {
+				float: 'left',
+				width: that.slideWidth
+			} );
+		}, 500 );
+
+		this.refreshButtons();
+	},
+	refreshButtons: function() {
+		this.$prevButton.removeClass( 'hide' );
+		this.$nextButton.removeClass( 'hide' );
+
+		var restSlides = this.amountOfSlides - this.activeIndex;
+
+		if( this.activeIndex === 0 )
+		{
+			this.$prevButton.addClass( 'hide' );
+		}
+
+		if( restSlides <= this.amountVisible )
+		{
+			this.$nextButton.addClass( 'hide' );
+		}
+	},
+	next: function() {
+		this.slideTo( this.activeIndex + 1 );
+	},
+	prev: function() {
+		this.slideTo( this.activeIndex - 1 );
+	},
+	slideTo: function( n ) {
+		var restSlides = this.amountOfSlides - n;
+
+		if( restSlides >= this.amountVisible && n >= 0 )
+		{
+			this.activeIndex = n;
+
+			this.$element.css( {
+				'transform': 'translateX( ' + -( this.activeIndex * this.slideWidth ) + 'px )'
+			} );
+
+			this.refreshButtons();
+		}
+	}
+};
+
+module.exports = Carousel;
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require( './Carousel.js' );
